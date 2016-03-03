28516164c7baaa7dfeb6c6b5e6c1e5b7028f6d1f
BestMatEver <mattkun@gmail.com>
2016-03-03 03:21:49 -0500

    renamed the folder. bad idea.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,10 @@ var ajaxData;
 var filmPlot;
 var movieInput;
 var orderedPlot;
+var subStringArray;
+//var gifArray;
+var plotArray;
+//var temp;
 
 $("#go").on("click", function(){
 
@@ -29,8 +33,10 @@ $("#go").on("click", function(){
     xhr.setRequestHeader('Authorization', 'Basic ' + base64Credentials);
   }
 
-  }).done(function(data){
-    ajaxData = data;
+}).done(function(data1){
+    ajaxData = data1;
+
+    var gifArray = getGifs();
 
     console.log(orderedPlot);
 
@@ -38,13 +44,23 @@ $("#go").on("click", function(){
     $("#titleSlide").attr('src', ajaxData[0].source.url);
     $("#titleCaption").text(movieInput);
 
-
-    var subStringArray;
-    var gifArray;
-
-    for(var i =0;i<10;i++)//gets 10 images, and sets up the strings for use
+    for(var k=0 ;k < 10;k++)
     {
+        $(".carousel-inner").append("<div class='item'><img src='" + gifArray[k] + "'><div class='carousel-caption'>" + orderedPlot[k] + "</div></div>");
+        // gifArray[k]
+    }//end append for loop.
 
+
+    //console.log(ajaxData);
+
+  });//end ajax call for initial Moviestuff
+
+
+function getGifs()
+{
+  var myarray;
+  for(var i = 0;i < 10;i++)//gets 10 images, and sets up the strings for use
+  {
         $.ajax({
           dataType: 'json',
           url: 'https://api.popkey.co/v2/media/search?q=' + orderedPlot[i] ,
@@ -54,34 +70,22 @@ $("#go").on("click", function(){
           xhr.setRequestHeader('Authorization', 'Basic ' + base64Credentials);
         }
 
-        }).done(function(data){
+      }).done(function(data2){
 
-          var temp;
-
-          temp = ((data[0].source.url));
-
-          gifArray[i] = temp;
-
+          console.log(data2[0].source.url);
+          // myarray.push(temp);
+          
            //pushes the gif url into an array.
         });//end ajax call for ordered array loop
-
     }//end the gif for loop. gifs captured.
-
-    for(var k=0;k<10;k++)
-    {
-        $(".carousel-inner").append("<div class='item'><img src='" +  + "'><div class='carousel-caption'>" + orderedPlot[k] + "</div></div>");
-        // gifArray[k]
-    }//end append for loop.
+    return myArray;
+}//end getgifs
 
 
-    //console.log(ajaxData);
-
-
-  });//end ajax call for initial Moviestuff
 
 function parsePlot(plot)
 {
-  var plotArray = plot.split(" ");
+  plotArray = plot.split(" ");
   console.log(plotArray);
 
   orderedPlot = plotArray.sort(function(a, b){
